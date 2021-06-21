c8a6758a5fe08c6c7f128476862872c91e30468d
chacha912 <dkwkdnwk912@gmail.com>
2021-06-21 20:32:08 +0900

    feat: Apply custom style for scrollbar

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('test');
+
+const $calendarDates = document.querySelector('.calendar');
+
+document.documentElement.style.setProperty(
+  '--scroll-width',
+  $calendarDates.offsetWidth - $calendarDates.clientWidth + 'px'
+);
