7f0fee465d5de40ac6a2dd88ffe4cd6580bcecfa
chacha912 <dkwkdnwk912@gmail.com>
2021-06-22 14:15:15 +0900

    feat: Style calendar nav & dropdown menu

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ document.documentElement.style.setProperty(
   '--scroll-width',
   $calendarDates.offsetWidth - $calendarDates.clientWidth + 'px'
 );
+
+document.querySelector('#dropdownCategoryBtn').addEventListener('click', e => {
+  document
+    .querySelector('.dropdown-category .dropdown-menu')
+    .classList.add('--show');
+});
+
+document.querySelector('.dropdown-calendar').addEventListener('click', e => {
+  console.log('click');
+  document.querySelector('.dropdown-menu').classList.add('--show');
+});
+
+document.querySelector('#dropdownCategoryBtn').addEventListener('blur', e => {
+  document
+    .querySelector('.dropdown-category .dropdown-menu')
+    .classList.remove('--show');
+});
+document.querySelector('#dropdownCalendarBtn').addEventListener('blur', e => {
+  document.querySelector('.dropdown-menu').classList.remove('--show');
+});
