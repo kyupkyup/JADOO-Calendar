03b299eda8270c8d2a987a957ad2d9c96fca8d36
Yourim Cha <81357083+chacha912@users.noreply.github.com>
2021-06-23 10:57:56 +0900

    feat: Markup & stlye modal-add (#5)
    - 아이템 추가 모달창 마크업 & 디자인
    - 아이템 추가 버튼 클릭시 모달창 열림 기능, transition 적용 (이벤트 위임)
    - 아이템 추가 확인 버튼 클릭시 입력한 폼 내용 확인

--- app.js
+++ app.js
@@ -26,3 +26,68 @@ document.querySelector('#dropdownCategoryBtn').addEventListener('blur', e => {
 document.querySelector('#dropdownCalendarBtn').addEventListener('blur', e => {
   document.querySelector('.dropdown-menu').classList.remove('--show');
 });
+
+const modalAdd = (() => {
+  let isActive = false;
+
+  const $modal = document.querySelector('.modal-add');
+  const $modalDim = document.querySelector('.modal-dim');
+  const $titleYear = document.querySelector('.modal-add .month');
+  const $titleMonth = document.querySelector('.modal-add .date');
+  const $itemDate = document.querySelector('.modal-add .modal-input-date');
+
+  return {
+    toggle(itemDate) {
+      isActive = !isActive;
+      $modal.classList.toggle('--show', isActive);
+      $modalDim.classList.toggle('--show', isActive);
+      document.body.classList.toggle('modal-open', isActive);
+
+      if (!isActive) return;
+      $titleYear.textContent = itemDate.slice(4, 6);
+      $titleMonth.textContent = itemDate.slice(6, 8);
+      $itemDate.value = itemDate;
+    },
+    close() {
+      isActive = false;
+      $modal.classList.remove('--show');
+      $modalDim.classList.remove('--show');
+      document.body.classList.remove('modal-open');
+    }
+  };
+})();
+
+const closest = ($startElem, targetClass, endClass) => {
+  let elem = $startElem;
+  while (!elem.classList.contains(targetClass)) {
+    if (elem.classList.contains(endClass)) {
+      return null;
+    }
+    elem = elem.parentNode;
+  }
+  return elem;
+};
+
+document.querySelector('.calendar-dates').addEventListener('click', e => {
+  const $selectedAddBtn = closest(e.target, 'item-add-btn', 'calendar-dates');
+  if (!$selectedAddBtn) return;
+
+  const itemDate = $selectedAddBtn.dataset.date;
+  modalAdd.toggle(itemDate);
+});
+
+document.querySelector('.modal-dim').addEventListener('click', () => {
+  modalAdd.close();
+});
+
+// 아이템 추가
+document.querySelector('.modal-add').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const itemDate = e.currentTarget.itemDate.value;
+  const itemCategory = e.currentTarget.itemCategory.value;
+  const itemType = e.currentTarget.itemType.value;
+  const itemContent = e.currentTarget.itemContent.value;
+
+  console.log(itemDate, itemCategory, itemType, itemContent);
+});
