43b9e54d55be8009ed393f5e4578a67fa57c6589
HyunJung <HyunJungC.Dev@gmail.com>
2021-06-24 11:02:21 +0900

    refactor: More clean code

--- dragAndDropFunction.js
+++ dragAndDropFunction.js
@@ -1,103 +1,118 @@
 // -------------------Drag and Drop Function-----------------------
-$calendarDates.addEventListener('mousedown', mousedownEvent => {
+$calendarDates.addEventListener('mousedown', e => {
   const initialMousePos = {
-    x: mousedownEvent.clientX,
-    y: mousedownEvent.clientY
+    x: e.clientX,
+    y: e.clientY
   };
-  let draggable = null;
-  if (mousedownEvent.target.classList.contains('icon-move')) {
-    mousedownEvent.target.style['pointer-events'] = 'none';
-    mousedownEvent.target.parentNode.style['pointer-events'] = 'none';
-    mousedownEvent.target.parentNode.parentNode.style['pointer-events'] =
-      'none';
-    mousedownEvent.target.parentNode.parentNode.parentNode.classList.add(
-      'dragging'
-    );
-    draggable = document.querySelector('.dragging');
+
+  const $selectedMoveBtn = closest(e.target, 'item-move-btn', 'calendar-dates');
+
+  if ($selectedMoveBtn) {
+    const draggable = closest(e.target, 'item', 'calendar-dates');
+    draggable.classList.add('dragging');
+
+    e.target.style['pointer-events'] = 'none';
+    e.target.parentNode.style['pointer-events'] = 'none';
+    e.target.parentNode.parentNode.style['pointer-events'] = 'none';
+
     [...document.querySelectorAll('.item-control-btn')].forEach(
       $itemControlBtn => {
         $itemControlBtn.classList.toggle('--invisible');
       }
     );
 
-    $calendarDates.onmousemove = e => {
+    const mouseMoveEventFunction = e => {
       draggable.style.transform = `translate3d(${
         e.clientX - initialMousePos.x
       }px, ${e.clientY - initialMousePos.y}px, 0)`;
     };
 
-    let afterElement = null;
-    let $container = null;
-    let prevAfterElemnt = null;
-    let prev$container = null;
+    $calendarDates.addEventListener('mousemove', mouseMoveEventFunction);
 
-    $calendarDates.onmouseover = e => {
-      if (prevAfterElemnt) prevAfterElemnt.style.border = 'none';
-      if (prev$container) prev$container.style.border = 'none';
+    let $container = null;
+    let $prevContainer = null;
+    let $nextElement = null;
+    let $prevNextElement = null;
+
+    const mouseOverEvent = e => {
+      if ($prevNextElement) $prevNextElement.style.border = 'none';
+      if ($prevContainer) $prevContainer.style.border = 'none';
 
       $container = e.target.closest('.items');
       if (!$container) return;
-      afterElement = e.target.closest('li');
-      draggable = document.querySelector('.dragging');
-      if (afterElement === draggable) {
+
+      $nextElement = e.target.closest('li');
+      if (draggable === $nextElement) return;
+      if ($nextElement) {
+        $nextElement.style['border-top'] = 'solid 5px gray';
+        $prevNextElement = $nextElement;
         return;
       }
 
-      if (afterElement == null) {
-        if (!$container.lastElementChild) {
-          $container.style['border-top'] = 'solid 5px gray';
-          prev$container = $container;
-        } else if (
-          $container.lastElementChild === draggable &&
-          draggable.previousElementSibling
-        ) {
-          draggable.previousElementSibling.style['border-bottom'] =
-            'solid 5px gray';
-          prev$container = draggable.previousElementSibling;
-        } else if ($container.lastElementChild !== draggable) {
-          $container.lastElementChild.style['border-bottom'] = 'solid 5px gray';
-          prev$container = $container.lastElementChild;
-        } else {
-          $container.style['border-top'] = 'solid 5px gray';
-          prev$container = $container;
-        }
-      } else {
-        afterElement.style['border-top'] = 'solid 5px gray';
-        prevAfterElemnt = afterElement;
+      const $lastElement = $container.lastElementChild;
+
+      // 이동할 위치에 아무 일정도 없는 경우
+      if (!$lastElement) {
+        $container.style['border-top'] = 'solid 5px gray';
+        $prevContainer = $container;
+        return;
       }
+
+      // 이동할 위치에  현재 이동중인 자신의 일정이 있는데
+      // 그게 이동할 위치의 마지막이고 자신보다 위에 일정이 있는 경우
+      const $prevElement = draggable.previousElementSibling;
+      if ($lastElement === draggable && $prevElement) {
+        $prevElement.style['border-bottom'] = 'solid 5px gray';
+        $prevContainer = $prevElement;
+        return;
+      }
+
+      // 이동할 위치에  현재 이동중인 자신의 일정이 있는데
+      // 그게 이동할 위치의 마지막이 아닌 경우
+      if ($lastElement !== draggable) {
+        $lastElement.style['border-bottom'] = 'solid 5px gray';
+        $prevContainer = $lastElement;
+        return;
+      }
+
+      // 이동할 위치에 현재 이동중인 자신의 일정만 있는 경우
+      $container.style['border-top'] = 'solid 5px gray';
+      $prevContainer = $container;
     };
 
-    const removeDraggingClass = () => {
-      $calendarDates.onmousemove = null;
+    $calendarDates.addEventListener('mouseover', mouseOverEvent);
+
+    const mouseUpEvent = () => {
+      draggable.classList.remove('dragging');
       draggable.style.transform = 'none';
-      if (prevAfterElemnt) prevAfterElemnt.style.border = 'none';
-      if (prev$container) prev$container.style.border = 'none';
-      mousedownEvent.target.parentNode.parentNode.parentNode.style.border =
-        'none';
-      mousedownEvent.target.parentNode.parentNode.parentNode.classList.remove(
-        'dragging'
-      );
-      document.removeEventListener('mouseup', removeDraggingClass);
-      $calendarDates.onmouseover = null;
-      mousedownEvent.target.style['pointer-events'] = 'auto';
-      mousedownEvent.target.parentNode.style['pointer-events'] = 'auto';
-      mousedownEvent.target.parentNode.parentNode.style['pointer-events'] =
-        'auto';
+
+      if ($prevNextElement) $prevNextElement.style.border = 'none';
+      if ($prevContainer) $prevContainer.style.border = 'none';
+
+      e.target.style['pointer-events'] = 'auto';
+      e.target.parentNode.style['pointer-events'] = 'auto';
+      e.target.parentNode.parentNode.style['pointer-events'] = 'auto';
+
       [...document.querySelectorAll('.item-control-btn')].forEach(
         $itemControlBtn => {
           $itemControlBtn.classList.toggle('--invisible');
         }
       );
 
+      $calendarDates.removeEventListener('mousemove', mouseMoveEventFunction);
+      $calendarDates.removeEventListener('mouseover', mouseOverEvent);
+      document.removeEventListener('mouseup', mouseUpEvent);
+
       if (!$container) return;
-      if (afterElement == null) {
+
+      if (!$nextElement) {
         $container.appendChild(draggable);
-      } else {
-        $container.insertBefore(draggable, afterElement);
+        return;
       }
+      $container.insertBefore(draggable, $nextElement);
     };
 
-    document.addEventListener('mouseup', removeDraggingClass);
+    document.addEventListener('mouseup', mouseUpEvent);
   }
 });
 // ----------------------------------------------------------------
