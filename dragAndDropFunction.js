464929b6621d29d6b309e87a48e14f50a2dfa813
HyunJung <HyunJungC.Dev@gmail.com>
2021-06-24 11:17:42 +0900

    refactor: Use closure

--- dragAndDropFunction.js
+++ dragAndDropFunction.js
@@ -27,92 +27,108 @@ $calendarDates.addEventListener('mousedown', e => {
       }px, ${e.clientY - initialMousePos.y}px, 0)`;
     };
 
-    $calendarDates.addEventListener('mousemove', mouseMoveEventFunction);
+    const mouseEventInMouseDownEvnet = (() => {
+      let $container = null;
+      let $prevContainer = null;
+      let $nextElement = null;
+      let $prevNextElement = null;
 
-    let $container = null;
-    let $prevContainer = null;
-    let $nextElement = null;
-    let $prevNextElement = null;
+      return {
+        mouseOverEvent(e) {
+          if ($prevNextElement) $prevNextElement.style.border = 'none';
+          if ($prevContainer) $prevContainer.style.border = 'none';
 
-    const mouseOverEvent = e => {
-      if ($prevNextElement) $prevNextElement.style.border = 'none';
-      if ($prevContainer) $prevContainer.style.border = 'none';
+          $container = e.target.closest('.items');
+          if (!$container) return;
 
-      $container = e.target.closest('.items');
-      if (!$container) return;
+          $nextElement = e.target.closest('li');
+          if (draggable === $nextElement) return;
+          if ($nextElement) {
+            $nextElement.style['border-top'] = 'solid 5px gray';
+            $prevNextElement = $nextElement;
+            return;
+          }
 
-      $nextElement = e.target.closest('li');
-      if (draggable === $nextElement) return;
-      if ($nextElement) {
-        $nextElement.style['border-top'] = 'solid 5px gray';
-        $prevNextElement = $nextElement;
-        return;
-      }
+          const $lastElement = $container.lastElementChild;
 
-      const $lastElement = $container.lastElementChild;
+          // 이동할 위치에 아무 일정도 없는 경우
+          if (!$lastElement) {
+            $container.style['border-top'] = 'solid 5px gray';
+            $prevContainer = $container;
+            return;
+          }
 
-      // 이동할 위치에 아무 일정도 없는 경우
-      if (!$lastElement) {
-        $container.style['border-top'] = 'solid 5px gray';
-        $prevContainer = $container;
-        return;
-      }
+          // 이동할 위치에 현재 이동중인 자신의 일정이 있는데
+          // 그게 이동할 위치의 마지막이고 자신보다 위에 일정이 있는 경우
+          const $prevElement = draggable.previousElementSibling;
+          if ($lastElement === draggable && $prevElement) {
+            $prevElement.style['border-bottom'] = 'solid 5px gray';
+            $prevContainer = $prevElement;
+            return;
+          }
 
-      // 이동할 위치에  현재 이동중인 자신의 일정이 있는데
-      // 그게 이동할 위치의 마지막이고 자신보다 위에 일정이 있는 경우
-      const $prevElement = draggable.previousElementSibling;
-      if ($lastElement === draggable && $prevElement) {
-        $prevElement.style['border-bottom'] = 'solid 5px gray';
-        $prevContainer = $prevElement;
-        return;
-      }
+          // 이동할 위치에 현재 이동중인 자신의 일정이 있는데
+          // 그게 이동할 위치의 마지막이 아닌 경우
+          if ($lastElement !== draggable) {
+            $lastElement.style['border-bottom'] = 'solid 5px gray';
+            $prevContainer = $lastElement;
+            return;
+          }
 
-      // 이동할 위치에  현재 이동중인 자신의 일정이 있는데
-      // 그게 이동할 위치의 마지막이 아닌 경우
-      if ($lastElement !== draggable) {
-        $lastElement.style['border-bottom'] = 'solid 5px gray';
-        $prevContainer = $lastElement;
-        return;
-      }
+          // 이동할 위치에 현재 이동중인 자신의 일정만 있는 경우
+          $container.style['border-top'] = 'solid 5px gray';
+          $prevContainer = $container;
+        },
+        mouseUpEvent() {
+          draggable.classList.remove('dragging');
+          draggable.style.transform = 'none';
 
-      // 이동할 위치에 현재 이동중인 자신의 일정만 있는 경우
-      $container.style['border-top'] = 'solid 5px gray';
-      $prevContainer = $container;
-    };
+          if ($prevNextElement) $prevNextElement.style.border = 'none';
+          if ($prevContainer) $prevContainer.style.border = 'none';
 
-    $calendarDates.addEventListener('mouseover', mouseOverEvent);
+          e.target.style['pointer-events'] = 'auto';
+          e.target.parentNode.style['pointer-events'] = 'auto';
+          e.target.parentNode.parentNode.style['pointer-events'] = 'auto';
 
-    const mouseUpEvent = () => {
-      draggable.classList.remove('dragging');
-      draggable.style.transform = 'none';
+          [...document.querySelectorAll('.item-control-btn')].forEach(
+            $itemControlBtn => {
+              $itemControlBtn.classList.toggle('--invisible');
+            }
+          );
 
-      if ($prevNextElement) $prevNextElement.style.border = 'none';
-      if ($prevContainer) $prevContainer.style.border = 'none';
+          $calendarDates.removeEventListener(
+            'mousemove',
+            mouseMoveEventFunction
+          );
+          $calendarDates.removeEventListener(
+            'mouseover',
+            mouseEventInMouseDownEvnet.mouseOverEvent
+          );
+          document.removeEventListener(
+            'mouseup',
+            mouseEventInMouseDownEvnet.mouseUpEvent
+          );
 
-      e.target.style['pointer-events'] = 'auto';
-      e.target.parentNode.style['pointer-events'] = 'auto';
-      e.target.parentNode.parentNode.style['pointer-events'] = 'auto';
+          if (!$container) return;
 
-      [...document.querySelectorAll('.item-control-btn')].forEach(
-        $itemControlBtn => {
-          $itemControlBtn.classList.toggle('--invisible');
+          if (!$nextElement) {
+            $container.appendChild(draggable);
+            return;
+          }
+          $container.insertBefore(draggable, $nextElement);
         }
-      );
+      };
+    })();
 
-      $calendarDates.removeEventListener('mousemove', mouseMoveEventFunction);
-      $calendarDates.removeEventListener('mouseover', mouseOverEvent);
-      document.removeEventListener('mouseup', mouseUpEvent);
-
-      if (!$container) return;
-
-      if (!$nextElement) {
-        $container.appendChild(draggable);
-        return;
-      }
-      $container.insertBefore(draggable, $nextElement);
-    };
-
-    document.addEventListener('mouseup', mouseUpEvent);
+    $calendarDates.addEventListener('mousemove', mouseMoveEventFunction);
+    $calendarDates.addEventListener(
+      'mouseover',
+      mouseEventInMouseDownEvnet.mouseOverEvent
+    );
+    document.addEventListener(
+      'mouseup',
+      mouseEventInMouseDownEvnet.mouseUpEvent
+    );
   }
 });
 // ----------------------------------------------------------------
